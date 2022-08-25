e66fd9265a92a3c9c618770083807a35c1989be3
Andrew Johnson <ajohnson238@huskers.unl.edu>
2022-08-25 13:32:26 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Andrew Johnson
+ * Date: 2022/23/08
+ *
+ * A simple hello world program in C
+ *
+ **/
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv)  {
+
+
+printf("Hello World!\n");
+printf("Andrew Johnson");
+
+return 0;
+
+ }
